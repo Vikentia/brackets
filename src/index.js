03bbbf635af234cd8a6bde1dbf6e07d45a51b10e
Victoria Kovaliova <victkovaliova@gmail.com>
2023-01-19 08:33:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrBrackets = str.split('')
+  let ind = 0
+  while (ind < str.length) {
+    bracketsConfig.forEach(el => {
+      if (el[1] === arrBrackets[ind] && el[0] === arrBrackets[ind - 1]) {
+        arrBrackets.splice(ind - 1, 2)
+        ind = 0
+      }
+    })
+    ind++
+  }
+  return !arrBrackets.length
 }
